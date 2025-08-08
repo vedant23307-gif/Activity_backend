45edf84fabff69e94e2d3ad5ae59486dd4e9c853
rushabhparamount <rushabh@paramountsoft.net>
2025-08-08 15:01:27 +0530

    change done

--- api.py
+++ api.py
@@ -3,53 +3,77 @@ from http.server import BaseHTTPRequestHandler,HTTPServer
 import pandas as pd
 import json
 def read(filename):
-    with open(filename,'r')as file:
-        data=file.read()
-    return data
+    try:
+        with open(filename,'r')as file:
+            data=file.read()
+        return data
+    except FileNotFoundError:
+        return None
 
-
-class Get(BaseHTTPRequestHandler):
+class Get(BaseHTTPRequestHandler): 
+    def res(self,data):
+        self.send_response(200)
+        self.send_header('Content-type','application/json')
+        self.end_headers()
+        self.wfile.write(data.encode())
     def do_GET(self):
         if self.path=='/json':
-            self.send_response(200)
-            self.send_header('Content-type','application/json')
-            self.end_headers()
-            repose=read('jsonfile.json')
-            self.wfile.write(repose.encode())
+            repose=json.dumps(read('jsonfile.json'))
+            
+            self.res(repose)
         if self.path=='/excel':
-            self.send_response(200)
-            self.send_header('Content-type','application/json')
-            self.end_headers()
-            response=pd.read_excel('ai.xlsx')
-            responses = response.to_json(orient='records')
-            self.wfile.write(responses.encode())
+            try:
+                response=pd.read_excel('ai.xlsx')
+                responses = response.to_json(orient='records')
+                self.res(responses)
+            except FileNotFoundError:
+                self.send_response(404)
+                self.end_headers()
+                self.wfile.write(json.dumps("file not found".encode()))
+            except pd.errors.EmptyDataError:
+                self.send_response(400,'the file is empty')
+                self.end_headers
+            except Exception as e:
+                self.send_response(500,'the excetion is{}',e)
+                self.end_headers()
         else:
             self.send_response(404,'file not found')
     def do_POST(self):
         if self.path == '/json/creat':
             length = int(self.headers.get('Content-Length', 0))
             data = self.rfile.read(length)
-            datamain = json.loads(data.decode('utf-8'))
-            datafinal=json.dumps(datamain)
-
-            with open('jsonfile.json', 'a') as f:
-                f.write(datafinal)
-            
-            self.send_response(200)
-            self.send_header('Content-type', 'application/json')
-            self.end_headers()
-            self.wfile.write(json.dumps({"message": "The file is created"}).encode())
+            try:
+                datamain = json.loads(data.decode('utf-8'))
+                datafinal=json.dumps(datamain)
+                with open('jsonfile.json', 'a') as f:
+                    f.write(datafinal)
+                message=json.dumps("the message get sucees fully")
+                self.res(message)
+            except json.JSONDecodeError:
+                self.send_response(404,'file not found')
+                self.end_headers()
+            except Exception as e:
+                self.send_response(500,'the excetion is{e}')
+                self.end_headers()
         elif self.path=='/excel/create':
-            contant=int(self.headers.get('Content-Length',0))
-            data=self.rfile.read(contant)
-            excel=json.loads(data)
-            df=pd.DataFrame(excel)
-            with pd.ExcelWriter('ai.xlsx', engine='openpyxl', mode='a', if_sheet_exists='overlay') as writer:
-              df.to_excel(writer, sheet_name='Sheet1', index=False,header=False, startrow=writer.sheets['Sheet1'].max_row)
-            self.send_response(200)
-            self.send_header('Content-type', 'application/json')
-            self.end_headers()
-            self.wfile.write(json.dumps({"message": "The file is created"}).encode())
+            try:
+                contant=int(self.headers.get('Content-Length',0))
+                data=self.rfile.read(contant)
+                excel=json.loads(data)
+                df=pd.DataFrame(excel)
+                with pd.ExcelWriter('ai.xlsx', engine='openpyxl', mode='a', if_sheet_exists='overlay') as writer:
+                    df.to_excel(writer, sheet_name='Sheet1', index=False,header=False, startrow=writer.sheets['Sheet1'].max_row)
+                massage=json.dumps("the massage get sucesfully")
+                self.res(message)
+            except json.JSONDecodeError:
+                self.send_response(404,"FileNotFoundError")
+                self.end_headers()
+            except FileNotFoundError:
+                self.send_response(404,'file not found')
+                self.end_headers()
+            except Exception as e:
+                self.send_response(500,'the excetion is{e}',)
+                self.end_headers()               
         else:
             self.send_response(404)
             self.end_headers()
