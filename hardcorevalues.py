9611dac358858eebe3894c028be493b6ee891cfd
Vedant <vedantpatel6353@gmail.com>
2025-08-11 15:31:55 +0530

    new file added

---
+++ hardcorevalues.py
@@ -0,0 +1,12 @@
+Get_json="/json"
+Get_excel="/excel"
+Post_json="/json/create"
+Post_excel="/excel/create"
+excelfile="ai.xlsx"
+jsonfile="jsonfile.json"
+success_code=200
+success_message="massage is successfully get"
+error_code=404
+error_message="File Not Found"
+port=8000
+
