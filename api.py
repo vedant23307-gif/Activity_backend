ffd99c4f70992806011d2bcd3cd7b0d7edb6c87c
Vedant <vedantpatel6353@gmail.com>
2025-08-12 23:00:43 +0530

    the looger is added

--- api.py
+++ api.py
@@ -1,10 +1,13 @@
 #this is the priogram related to accseing the file for chrom using the http 
 from http.server import BaseHTTPRequestHandler,HTTPServer
 import pandas as pd
-import hardcorevalues as static
-from helperfunction import Response as help
+import constants as static
+from utils import response as help
 import json
 import logging
+import os
+log_path = os.path.join(os.path.dirname(__file__), "log.log")
+logging.basicConfig(filename=log_path,filemode='a',level=logging.INFO)
 def read(filename):
     try:
         with open(filename,'r')as file:
@@ -15,58 +18,65 @@ def read(filename):
 #this class handel all the http requests
 class Get(BaseHTTPRequestHandler):   
     def do_GET(self):
-        if self.path==static.Get_json:
+        if self.path==static.Get_json_url:
             repose=json.dumps(read(static.jsonfile))
 
             #the helper function are called
+            logging.info("the json is called")
             help.response(self,repose)
-        elif self.path==static.Get_excel:
+        elif self.path==static.Get_excel_url:
             try:
                 response=pd.read_excel(static.excelfile)
                 responses = response.to_json(orient='records')
+                logging.info("the excel is called")
                 #the helper function are called
                 help.response(self,responses)
             except FileNotFoundError:
                 self.send_response(static.error_code)
+                logging.info("the file not found excepion raised")
                 self.end_headers()
                 self.wfile.write(json.dumps(static.error_message).encode())
             except pd.errors.EmptyDataError:
                 self.send_response(400)
+                logging.info("the pd Empty excepion raised")
                 self.end_headers()
                 self.wfile.write(json.dumps("file is empty".encode()))
             except Exception as e:
                 self.send_response(500)
+                logging.info("the exception is {e} raised")
                 self.end_headers()
                 self.wfile.write(json.dumps("the exception is {e}".encode()))
         else:
             self.send_response(static.error_code)
+            logging.info("the file not found for get")
             self.end_headers()
             self.wfile.write(json.dumps(static.error_message).encode())
     def do_POST(self):
-        if self.path == static.Post_json:
+        if self.path == static.Post_json_url:
             length = int(self.headers.get('Content-Length', 0))
             data = self.rfile.read(length)
             try:
                 datamain = json.loads(data.decode('utf-8'))
                 datafinal=json.dumps(datamain)
-                logging.basicConfig(filename="log.log",filemode='w',level=logging.info)
                 logging.info('the message revered')
                 with open(static.jsonfile, 'w') as f:
                     f.write(datafinal)
+                    logging.info("data printed succesfully")
                 message=json.dumps(static.success_message)
                 #the helper function are called
-                logging.basicConfig(filename="log.log",filemode='w',level=logging.info)
-                logging.info('the message revered')
+                logging.info("the response is called ofr json create")
                 help.response(self,message)
             except json.JSONDecodeError:
                 self.send_response(static.error_code)
+                logging.info("teh json decorde exception")
                 self.end_headers()
                 self.wfile.write(json.dumps(static.error_message).encode())
             except Exception as e:
                 self.send_response(500,'the excetion is{e}')
+                logging.info("the exception is {e} raised")
                 self.end_headers()
                 self.wfile.write(json.dumps("the exception is {e}".encode()))
-        elif self.path==static.Post_excel:
+        elif self.path==static.Post_excel_url:
             try:
                 contant=int(self.headers.get('Content-Length',0))
                 data=self.rfile.read(contant)
@@ -80,18 +90,22 @@ class Get(BaseHTTPRequestHandler):
                 help.response(message)
             except json.JSONDecodeError:
                 self.send_response(404,"FileNotFoundError")
+                logging.info("the json decorde exception")
                 self.end_headers()
                 self.wfile.write(json.dumps("there is problem in the json file youn send".encode()))
             except FileNotFoundError:
                 self.send_response(404,'file not found')
+                logging.info("the file not found excepion raised")
                 self.end_headers()
                 self.wfile.write(json.dumps("file not found".encode()))
             except Exception as e:
                 self.send_response(500,'the excetion is{e}',)
+                logging.info("the exception is {e} raised")
                 self.end_headers() 
                 self.wfile.write(json.dumps("the exception is {e}".encode()))              
         else:
             self.send_response(404)
+            logging.info("teh file is not found for post")
             self.end_headers()
 
 def run(server=HTTPServer,clas=Get,port=8000):
